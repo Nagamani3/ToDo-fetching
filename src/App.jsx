98704e99f3f60fd3545a691138336bbd808ae81e
unknown <nagamanihoogar619@gmail.com>
2022-01-13 16:00:58 +0530

    this is all about fetching data and paginating

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from './Components/Post'
+import "./index.css"
+
+const App = () => {
+    return (
+        <div>
+            <Post/>
+        </div>
+    )
+}
+
+export default App
